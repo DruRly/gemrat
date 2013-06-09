5d4b66b3802cd8664512c1f778a1783525edd3db
Dru <drurly@gmail.com>
2013-06-09 13:06:07 -0400

    Fetch and format gem name

--- lib/gemrat.rb
+++ lib/gemrat.rb
@@ -1,5 +1,21 @@
 require "gemrat/version"
 
 module Gemrat
-  # Your code goes here...
+  def fetch_gem name
+    gems_response = `gem search -r #{name}`
+    names_array = gems_response.split(/\n/)
+    match = names_array.find { |n| /^#{name} / =~ n }
+    gem = ("gem " + match).gsub(/[()]/, "'")
+    gem.gsub(/#{name}/, "'#{name}',")
+  end
+
+  def convert_gem name_version
+    name_version
+  end
+
+  def add_gem name
+    #Fetch gem
+    #Open gemfile
+    #Add gem
+  end
 end
--- spec/gemrat_spec.rb
+++ spec/gemrat_spec.rb
@@ -4,15 +4,31 @@ describe Gemrat do
     test_gemfile = File.new("TestGemfile", "w")
     test_gemfile.puts ("https://rubygems.org'\n\n# Specify your gem's dependencies in gemrat.gemspec\ngem 'rspec', '2.13.0'\n")
     test_gemfile.close
+
+    class DummyClass
+    end
+
+    @dummy_class = DummyClass.new
+    @dummy_class.extend(Gemrat)
   end
 
   after do
     File.delete("TestGemfile")
   end
 
-  it "example" do
+  it "adds the latest version of a gem to the file" do
     `gemrat sinatra`
     gemfile_contents = File.open('TestGemfile').read
     gemfile_contents.should include("gem 'sinatra', '1.4.2'")
+  end
+
+  it "returns a message if it can't find rubygems" do
+
+  end
+
+  describe "#fetch_gem" do
+    it "returns latest version of gem" do
+      @dummy_class.fetch_gem("sinatra").should == "gem 'sinatra', '1.4.3'"
+    end
   end
 end
